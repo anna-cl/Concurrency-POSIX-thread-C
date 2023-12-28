50ebc1d9971b007ed8b666722e26c8721eaadb6e
Anna Chaoer Liang <anna_liang_2@sfu.ca>
2023-12-27 23:55:05 -0800

    consumer producer

---
+++ consumer_producer.c
@@ -0,0 +1,8 @@
+// https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/ProdCons.html
+// 8.3.1. Producer-Consumer with Unbounded Queue
+
+
+
+
+
+// https://www.eg.bucknell.edu/~xmeng/Course/CS4334/Example/Pthread/producer-consumer.c
